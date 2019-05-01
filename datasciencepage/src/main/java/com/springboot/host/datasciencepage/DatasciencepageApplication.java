4ecd86d309b071167398a815381f7101c3f20108
singhalsrishty <singhalsrishty1@gmail.com>
2019-05-01 14:49:48 +0530

    data science project - initial commit

---
+++ datasciencepage/src/main/java/com/springboot/host/datasciencepage/DatasciencepageApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.host.datasciencepage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatasciencepageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatasciencepageApplication.class, args);
+	}
+
+}
---
+++ datasciencepage/src/test/java/com/springboot/host/datasciencepage/DatasciencepageApplicationTests.java
@@ -0,0 +1,16 @@
+package com.springboot.host.datasciencepage;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DatasciencepageApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
